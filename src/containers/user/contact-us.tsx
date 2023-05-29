ddf45a73822caf7e082350a46b3c8bec6cd2c6b0
NaflaSale <n.alanazi@thegarage.sa>
2023-05-29 15:14:23 +0300

    Update

--- src/containers/user/contact-us.tsx
+++ src/containers/user/contact-us.tsx
@@ -8,9 +8,11 @@ export default function ContactUs(){
        <h1 className="text-[40px] flex justify-center pt-20 sm:text-[30px] font-title text-darkBlue pb-[22px]">
         {t("home.page.cotact")}
       </h1>
+      <hr></hr>
       <div className="text-[30px] flex flex-col space-y-4  items-center pt-20 sm:text-[20px]  text-darkBlue pb-[22px]">
       <p>email@gmail.com</p>
       <p>966532145670+</p>
+      
       </div>
     </div>
   )
